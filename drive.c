81986d53c95a1e566d0e9cf24a8df6ba68bfb374
Elytrus <moses@mosesxu.net>
2019-01-09 16:33:10 -0500

    faraz when r u coming back from saudi we need yougit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .

--- drive.c
+++ drive.c
@@ -14,7 +14,31 @@ motor[motorbr] = (-(vbr) / REDUCE)
 const int REDUCE = 1;
 const bool LEFT = true; // Enables left-handed ness
 
-int up, down, left, right;
+int up, down, left, right, uplift, downlift, rotateleft, rotateright;
+
+void lift_motor(int state){
+	if(state = -1){ // Down
+		//
+	}
+	else if(state == 0){ // None
+		//
+	}
+	else{ // state == 1 | Up
+		//
+	}
+}
+
+void rotate_motor(int state){
+	if(state = -1){ // Down
+		//
+	}
+	else if(state == 0){ // None
+		//
+	}
+	else{ // state == 1 | Up
+		//
+	}
+}
 
 task main(){
 	for(;;){
@@ -23,20 +47,46 @@ task main(){
 		left = LEFT ? vexRT[Btn7L] : vexRT[Btn8L];
 		right = LEFT ? vexRT[Btn7R] : vexRT[Btn8R];
 
+		uplift = vexRT[Btn8U];
+		downlift = vexRT[Btn8D];
+
+		rotateleft = vexRT[Btn8L];
+		rotateright = vexRT[Btn8R];
+
 		if(up){
 			SET_MOTOR(127, 127, 127, 127);
 		}
 		else if(down){
 			SET_MOTOR(-127, -127, -127, -127);
 		}
-		else if(left){
+		else if(right){
 			SET_MOTOR(-127, 127, -127, 127);
 		}
-		else if(right){
+		else if(left){
 			SET_MOTOR(127, -127, 127, -127);
 		}
 		else{
 			SET_MOTOR(0, 0, 0, 0);
+		}
+
+		if(uplift){
+			lift_motor(1);
+		}
+		else if(downlift){
+			lift_motor(-1);
+		}
+		else{
+			lift_motor(0);
+		}
+
+		if(rotateright){
+			rotate_motor(1);
+		}
+		else if(rotateleft){
+			rotate_motor(-1);
+		}
+		else{
+			rotate_motor(0);
 		}
 	}
 }
